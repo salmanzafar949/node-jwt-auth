3f80a93718a33255059cc31c844044e579b5f951
Salman Zafar <salman.zafar@innowi.com>
2020-12-24 15:24:42 +0500

    conditonal rendering in view

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -26,14 +26,13 @@ const checkUser = (req, res, next) => {
     const token = req.cookies.jwt;
 
     if (token){
-        jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+        jwt.verify(token, process.env.JWT_SECRET, async (err, decodedToken) => {
             if (err){
                 console.log(err.message)
                 res.locals.user = null
                 next()
             }else{
-                let user = User.findById(decodedToken.id)
-                res.locals.user = user
+                res.locals.user = await User.findById(decodedToken.id)
                 next()
             }
         })
